5bf353255ed85cff985fc220b26a1ded71b0fa86
unknown <labmin@LABSEC09---18FC.storms.css.edu>
2015-04-10 14:06:23 -0500

    Initial commit

---
+++ app/src/main/java/edu/cfilsaimecss/mapdemo/MapDemoActivity.java
@@ -0,0 +1,91 @@
+package edu.cfilsaimecss.mapdemo;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MapDemoActivity extends FragmentActivity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_demo);
+        setUpMapIfNeeded();// is just in case your app may be running in the background.
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(46.815678, -92.106570)).title("The College of Saint Scholastica"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(45.068721, -92.98932)).title("Carl's House"));
+
+        double latitude;
+        double longitude;
+        List<Address> geocodeMatches = null;
+
+        try
+        {
+            geocodeMatches = new Geocoder(this).getFromLocationName("2629 South Shore blvd, White Bear lake,MN",1);
+        }
+        catch (IOException e)
+        {
+            // TODO AUTO-generated catch block
+            e.printStackTrace();
+        }
+        // Make sure an address was returned.
+        if (!geocodeMatches.isEmpty()){
+            latitude = geocodeMatches.get(0).getLatitude();
+            longitude = geocodeMatches.get(0).getLongitude();
+            mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Carl's House"));
+        }
+    }
+}
